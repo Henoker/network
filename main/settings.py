74ce87588be538f9077fe71339b776641f03ab2c
Henoker <hennybany@gmail.com>
2023-10-19 22:54:04 +0300

    feat: added new app posts

--- main/settings.py
+++ main/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     #Local
     "accounts.apps.AccountsConfig",
+    "posts.apps.PostsConfig",
     # Third Party Apps
     "rest_framework",
   
